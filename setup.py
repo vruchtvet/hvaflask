c8baaba296572b36aa96742afdfb21654db67c9c
Hans Scholte <scholte.h@gmail.com>
2020-11-10 14:47:38 +0100

    Added setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name='flaskr',
     version='1.0.0',
+    description='HvA Scripties implemented with flask',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
     ],
+    python_requires='>=3'
 )
